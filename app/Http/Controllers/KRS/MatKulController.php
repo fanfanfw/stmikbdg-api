88a111ad1a60b1d34a9d2db41e97f9cfbf93305e
suhaefifzn <suhaefi21@gmail.com>
2024-08-29 15:21:28 +0700

    update list krs matkul

--- app/Http/Controllers/KRS/MatKulController.php
+++ app/Http/Controllers/KRS/MatKulController.php
@@ -137,16 +137,17 @@ class MatKulController extends Controller
 
             // get latest krs matkul
             $allKrsMatkulLast = $latestKRS->krsMatkul()->get();
+            $pluckedAllKrsMatkulLast = count($allKrsMatkulLast) > 0 ? $allKrsMatkulLast->pluck('mk_id') : collect(['mk_id' => null]);
             $allKrsMatkul = $latestKRS ? $latestKRS->krsMatkul()->get() : $mappedListMatkulWithNilaiAkhir;
             $allMkIdKrsMatkul = $allKrsMatkul->pluck('mk_id')->toArray();
             $mappedWithKrsMatkul = $mappedListMatkulWithNilaiAkhir
-                ->map(function ($mk) use ($allMkIdKrsMatkul, $collectMkIdDiselenggarakan, $allKrsMatkulLast) {
+                ->map(function ($mk) use ($allMkIdKrsMatkul, $collectMkIdDiselenggarakan, $pluckedAllKrsMatkulLast) {
                     $isSameSmt = $mk['smt'] === $this->currentSemester['smt'] ?? false;
 
                     if (in_array($mk['mk_id'], $allMkIdKrsMatkul)) {
                         $mk['krs'] = [
                             'is_aktif' => $collectMkIdDiselenggarakan->contains($mk['mk_id']) ? true : false, // sebelumnya $isSameSmt
-                            'is_checked' => $allKrsMatkulLast->contains($mk['mk_id']) ? true : false, // sementara
+                            'is_checked' => $pluckedAllKrsMatkulLast->contains($mk['mk_id']) ? true : false, // sementara
                         ];
                     } else {
                         $mk['krs'] = [
