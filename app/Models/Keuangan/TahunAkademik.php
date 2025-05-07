c8e2b21b9b8a990fdb8624d34eb34a75c4e7eceb
ZiyadZK <kakangtea74@gmail.com>
2025-05-08 00:01:25 +0700

    push for new routes, models, and controllers

---
+++ app/Models/Keuangan/TahunAkademik.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models\Keuangan;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class TahunAkademik extends Model
+{
+    use HasFactory;
+
+    protected $table = 'keuangan.m_thn_akademik';
+    protected $connection;
+
+    public $primaryKey = 'id_thn_akademik';
+    protected $fillable = [
+        'thn_akademik',
+        'ganjil_mulai',
+        'ganjil_akhir',
+        'genap_mulai',
+        'genap_akhir',
+        'antara_mulai',
+        'antara_akhir',
+        'status'
+    ];
+    public $increment = true;
+    public $timestamps = true;
+
+    public function __construct() {
+        $this->connection = config('myconfig.database.first_connection');
+    }
+
+    public function scopeGetTahunAkademik(Builder $query, array $filters = []) {
+        // $record = DB::table('keuangan.m_tahun_akademi')->first();
+
+        // return $record;
+
+        foreach ($filters as $key => $value) {
+            if (in_array($key, $this->fillable)) {
+                $query->where($key, $value);
+            }
+        }
+    
+        return $query->get();
+    }
+}
