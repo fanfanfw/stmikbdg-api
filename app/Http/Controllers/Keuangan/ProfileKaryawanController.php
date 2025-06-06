558a4085be576bdb61bb59df074a67ed5ef029ef
zidan <muhamadzidanfrjtllh@gmail.com>
2025-06-06 16:30:23 +0700

    on going profile karyawan

--- app/Http/Controllers/Keuangan/ProfileKaryawanController.php
+++ app/Http/Controllers/Keuangan/ProfileKaryawanController.php
@@ -77,7 +77,88 @@ class ProfileKaryawanController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try{
+
+            $validated = $request->validate([
+                'nidn_nuptk' => 'required|string|max:50',
+                'nik' => 'string|max:50',
+                'nama' => 'required|string|max:50',
+                'email' => 'required|string|max:75',
+                'no_telepon' => 'required|string|max:50',
+                'alamat' => 'required|string',
+                'pendidikan_terakhir' => 'required|string',                                                                                                                                                                                                                                             
+                'tmt' => 'required|date',                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+                'no_rekening' => 'string',
+                'nama_bank' => 'string',
+                'status_menikah' => 'required|boolean',
+                'jabatan' => 'integer',
+                'golongan' => 'integer',
+                'fungsional' => 'integer',
+                'status' => 'integer',
+                'beban_max_sks' => 'integer',                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+                'honor_per_sks' => 'integer',
+                'tunjangan_yayasan' => 'integer',
+                'tunjangan_fungsional' => 'integer',
+                //validasi tunangan lainnya (keluarga, makan, transport)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+              
+            ],[
+                'nidn_nuptk.required' => 'NIDN/NUPTK wajib diisi.',
+                'nidn_nuptk.string'   => 'NIDN/NUPTK harus berupa teks.',
+                'nidn_nuptk.max'      => 'NIDN/NUPTK maksimal 50 karakter.',
+
+                'nik.string' => 'Kolom nik harus berupa teks.',
+                'nik.max'    => 'Kolom nik maksimal 50 karakter.',
+
+                'nama.required' => 'Kolom nama wajib diisi.',
+                'nama.string'   => 'Kolom nama harus berupa teks.',
+                'nama.max'      => 'Kolom D2 maksimal 50 karakter.',
+
+                'email.required' => 'Kolom email wajib diisi.',
+                'email.string'   => 'Kolom email harus berupa teks.',
+                'email.max'      => 'Kolom email maksimal 75 karakter.',
+
+                'no_telpon.required' => 'Kolom no telpon wajib diisi.',
+                'no_telpon.string'   => 'Kolom no telpon harus berupa teks.',
+                'no_telpon.max'      => 'Kolom no telpon maksimal 50 karakter.',
+
+                'alamat.required' => 'Kolom alamat wajib diisi.',
+                'alamat.string'   => 'Kolom alamat harus berupa teks.',
+
+                'pendidikan_terakhir.required' => 'Kolom pendidikan_terakhir wajib diisi.',
+                'pendidikan_terakhir.string'   => 'Kolom pendidikan_terakhir harus berupa teks.',
+
+                'tmt.required' => 'Kolom tmt wajib diisi.',
+                'tmt.date'     => 'Kolom tmt harus berupa tanggal yang valid.',
+
+                'no_rekening.string' => 'Kolom no rekening harus berupa teks.',
+                'nama_bank.string' => 'Kolom nama bank harus berupa teks.',
+
+                'status_menikah.required' => 'Kolom status menikah wajib diisi.',
+                'status_menikah.boolean'  => 'Kolom status menikah harus berupa true atau false.',
+            ]);
+
+            $data = ProfileKaryawan::create($validated);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Data berhasil ditambahkan.',
+                'data' => $data
+            ],201);
+
+        }catch (\Illuminate\Validation\ValidationException $error){
+            return response()->json([
+                'success' => false,
+                'message' =>  'Terdapat kesalahan pada input Anda. Mohon periksa atau lengkapi kembali',
+                'errors' => $error->errors(),
+            ], 422);
+        } catch (\Exception $error) {
+           
+            return response()->json([
+                'success' => false,
+                'message' => 'Terjadi kesalahan saat menyimpan data.',
+                'error' => $error->getMessage() // hapus di production
+            ], 500);
+        }
     }
 
     /**
@@ -96,3 +177,6 @@ class ProfileKaryawanController extends Controller
         //
     }
 }
+
+
+
