67fa5741f3265388d1a65194397efc1953c7a0c0
iamfizzer <bagindafizzer99@gmai.com>
2025-06-20 03:03:12 +0700

    SIKPS Leni First

--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,9 @@ require __DIR__.'/api/rekap.php';
 // ? Berita Acara
 require __DIR__.'/api/berita.php';
 
+// ? SIKPS V2
+require __DIR__.'/api/sikpsv2.php';
+
 // ? Additional Routes (Tambahan)
 require __DIR__.'/api/additional.php';
 require __DIR__.'/api/file.php';
