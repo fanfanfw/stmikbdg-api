be7cf10ef5f4c8f43b8bc7e6f773511f851d5f96
STMIK BANDUNG <developer.stmikbandung@gmail.com>
2025-09-04 15:55:11 +0700

    Update MhsController.php

--- app/Http/Controllers/Keuangan/MhsController.php
+++ app/Http/Controllers/Keuangan/MhsController.php
@@ -194,7 +194,7 @@ class MhsController extends Controller
                 'kodeMKDiAmbil' => $kodeMKDiambil
             ];
 
-            return response()->json($data);
+            return response()->json($semester);
 
             $merged_data = collect(); // mulai dengan empty collection
 
