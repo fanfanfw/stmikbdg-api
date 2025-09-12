6614f6b905bafd3b7831551912b5de9082161497
ZiyadZK <kakangtea74@gmail.com>
2025-09-12 10:36:34 +0700

    debugging purpose

--- app/Http/Controllers/Users/UserController.php
+++ app/Http/Controllers/Users/UserController.php
@@ -146,15 +146,15 @@ class UserController extends Controller {
 
             $keuangan = null;
 
-            return response()->json([
-                'status' => 'success',
-                'message' => 'Berhasil mengambil data profile',
-                'data' => [
-                    'profile' => $user,
-                    'account' => $account,
-                    'keuangan' => $keuangan
-                ]
-            ], 200);
+            // return response()->json([
+            //     'status' => 'success',
+            //     'message' => 'Berhasil mengambil data profile',
+            //     'data' => [
+            //         'profile' => $user,
+            //         'account' => $account,
+            //         'keuangan' => $keuangan
+            //     ]
+            // ], 200);
 
             if(isset($account['is_mhs'])) {
                 if($account['is_mhs']) {
