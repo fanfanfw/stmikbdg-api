97ebb339668b5e69d307bdd3b5a124e5bc0398ac
ZiyadZK <kakangtea74@gmail.com>
2025-09-14 14:11:52 +0700

    debugging log

--- app/Http/Controllers/KRS/KRSDosenController.php
+++ app/Http/Controllers/KRS/KRSDosenController.php
@@ -39,6 +39,12 @@ class KRSDosenController extends Controller
             $jurusanMahasiswa = $mahasiswa->jurusan()->first();
             $krsMahasiswa = $mahasiswa->krs()->first();
             $krsMatkulDipilih = $krsMahasiswa->krsMatkul()->get();
+            $this->debug_log([
+                'mahasiswa' => $mahasiswa,
+                'jurusanMahasiswa' => $jurusanMahasiswa,
+                'krsMahasiswa' => $krsMahasiswa,
+                'krsMatkulDipilih' => $krsMatkulDipilih,
+            ]);
             $setKRSData = self::setKRSData($jurusanMahasiswa, $krsMahasiswa, $krsMatkulDipilih, $mahasiswa['mhs_id']);
 
             return $this->successfulResponseJSON([
